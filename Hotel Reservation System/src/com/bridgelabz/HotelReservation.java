1f833b9a58fe0fbb3ab7d2cebdaaa4ddcc7463ad
Sankett65 <127722641+Sankett65@users.noreply.github.com>
2023-10-29 00:31:47 +0530

    Update HotelReservation.java

--- Hotel Reservation System/src/com/bridgelabz/HotelReservation.java
+++ Hotel Reservation System/src/com/bridgelabz/HotelReservation.java
@@ -14,6 +14,63 @@ public class HotelReservation {
         hotelList.add(hotel);
     }
 
+    public List<String> toGetDay(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\nEnter the Starting Date in YYYY-MM-DD");
+        System.out.print("Enter the Year:- ");
+        int year=sc.nextInt();
+        System.out.print("Enter the Month:- ");
+        int month=sc.nextInt();
+        System.out.print("Enter the Day:- ");
+        int day=sc.nextInt();
+
+        System.out.println("\nEnter the Ending Date YYYY-MM-DD");
+        System.out.print("Enter the Year:- ");
+        int year1=sc.nextInt();
+        System.out.print("Enter the Month:- ");
+        int month1=sc.nextInt();
+        System.out.print("Enter the Day:- ");
+        int day1=sc.nextInt();
+
+        LocalDate Startdate1=LocalDate.of(year,month,day);
+        LocalDate Enddate1=LocalDate.of(year1,month1,day1);
+
+        long numOfDaysBetween = ChronoUnit.DAYS.between (Startdate1, Enddate1)+1;
+        List<String> list= new ArrayList<>();
+        for (int i =0;i<numOfDaysBetween;i++){
+            list.add(String.valueOf(Startdate1.plusDays(i).getDayOfWeek()));
+
+        }
+
+        return list;
+    }
+
+
+    public void minimumRates(List<String> list){
+        int sum1=0;
+        int sum2=0;
+        int sum3=0;
+        for (Hotel hotel : hotelList) {
+            switch (hotel.getHotelName()) {
+                case "Lakewood" -> sum1 = hotel.rateOfHotel(list);
+                case "Bridgewood" -> sum2 = hotel.rateOfHotel(list);
+                case "Ridgewood" -> sum3 = hotel.rateOfHotel(list);
+            }
+        }
+
+        if (sum1<sum2 && sum1<sum3){
+            System.out.println("\nThis Rate is for Hotel Lakewood");
+            System.out.println("Minimum Rates :- "+sum1+"$");
+        }else  if (sum2<sum1 && sum2<sum3){
+            System.out.println("\nThis Rate is for Hotel Bridgewood");
+            System.out.println("Minimum Rates :- "+sum2+"$");
+        }else {
+            System.out.println("\nThis Rate is for Hotel Ridgewood");
+            System.out.println("Minimum Rates :- "+sum3+"$");
+        }
+    }
+
     public void printHotelList(){
         for (Hotel h : hotelList){
             System.out.println(h);
@@ -39,6 +96,7 @@ public class HotelReservation {
         System.out.println("List of Hotel Available");
         hotelReservation.printHotelList();
 
-
+        
+        hotelReservation.minimumRates(hotelReservation.toGetDay());
     }
 }
